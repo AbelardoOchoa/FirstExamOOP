9af5acf1b9e078ddf4f030dc51b0364544a2dcca
AbelardoOchoa <abelardoo@uninorte.edu.co>
2022-10-06 09:41:45 -0500

    Final

--- code_to_run.py
+++ code_to_run.py
@@ -1,7 +1,5 @@
 from desarrollo import Buscaminas, Tablero, Minas, Jugador
 posada = Jugador()
-juego = Buscaminas()
-tablero = Tablero()
 
 while(posada.victoria):
     posada.jugar()
--- desarrollo.py
+++ desarrollo.py
@@ -1,35 +1,45 @@
-class Buscaminas():
-    def __init__(self) -> None:
-        self.interfaz = Tablero()
-
-class Tablero():
-    def __init__(self) -> None:
-        self.casilla = [[0, 1, 0, 1, 0],[1, 0, 1, 0, 1]]
-    
-
 class Minas():
+    def __init__(self):
+        self.victoria = True
     def perder(self) -> None:
         """
         Termina el juego.
         """
+        print("Perdiste, has estallado.")
         self.victoria = False
     def boom(self) -> None:
         """
         Le informa al jugador que seleccionó una mina y estalló.
         """
         print("Perdiste, has estallado.")
-            
 
-class Jugador():
+class Tablero(Minas):
+    def __init__(self) -> None:
+        self.casilla = []
+        
+class Buscaminas(Tablero):
+    def __init__(self) -> None:
+        self.interfaz = Tablero()
+    
+class Jugador(Buscaminas):
     def __init__(self) -> None:
         self.victoria = True
+        self.casilla=   [[1, 0, 0, 0, 0],
+                        [0, 1, 0, 0, 0], 
+                        [0, 0, 1, 0, 0], 
+                        [0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 1]]
     
     def jugar(self) -> None:
         """
         Comienza una partida.
         """
-        fila = int(input("Digite un número de fila."))
-        columna = int(input("Digite un número de columna."))
+        fila = int(input("Digite un número de fila entre 0 y 4."))
+        while(fila > -1 and fila > 4):
+            fila = int(input("Digite un número de fila entre 0 y 4."))
+        columna = int(input("Digite un número de columna entre 0 y 4."))
+        while(columna > -1 and columna > 4):
+            columna = int(input("Digite un número de columna entre 0 y 4."))
         if self.casilla[fila][columna] == 1:
             self.perder()
         elif self.casilla[fila][columna] == 2:
